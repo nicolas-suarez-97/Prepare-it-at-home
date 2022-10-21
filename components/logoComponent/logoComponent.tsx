4a11cee11e2ee1a7790ac7009edce7b6b00d4836
nicolas-suarez-97 <nicolas.suarez10@hotmail.com>
2022-10-20 23:58:07 -0500

    Adjust assets

--- components/logoComponent/logoComponent.tsx
+++ components/logoComponent/logoComponent.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./logo.module.scss"
-import Link from "next/link";
 
 const Logo = () => {
     return (
-        <a className={styles.logo}>
-            <h1>Preparalo en Casa</h1>
-        </a>
+        <img
+            src="/img/cocteleriaDeAutor/logo.png"
+            alt="prepare it at home"
+            className={styles.logo}
+        />
     );
 }
 
