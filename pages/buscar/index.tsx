9865e14e357dcd3bc893725745f034cc278e48cc
nicolas-suarez-97 <nicolas.suarez10@hotmail.com>
2023-05-30 23:43:17 -0500

    Fix search page

--- pages/buscar/index.tsx
+++ pages/buscar/index.tsx
@@ -2,19 +2,27 @@ import React from 'react'
 import styles from './searchRecipe.module.scss'
 import { Layout } from '../../sections'
 import { Separator, TitleComponent } from '../../components'
+import NotFound from '../404'
+import Image from 'next/image'
 
-const SearchRecipe = ({query}: any) => {
+const SearchRecipe = ({ query }: any) => {
   return (
     <Layout>
-      <TitleComponent title={`Busqueda: ${query}`} align={'center'} />
-      <Separator alignment={'center'}/>
+      <TitleComponent title={ `Busqueda: ${ query }` } align={ 'center' }/>
+      <Separator alignment={ 'center' }/>
+      <div className={ styles['not-found'] }>
+        <div className={ styles['not-found__image'] }>
+          <Image src={ '/img/undraw_barbecue.svg' } alt={ 'Not found' } fill/>
+        </div>
+        <p>Ups... No hay resultados para <b>{query}</b></p>
+      </div>
     </Layout>
-  );
+  )
 }
 
 export async function getServerSideProps({ query }: any) {
 
-  return { props: {query: query.query} }
+  return { props: { query: query.query } }
 }
 
-export default SearchRecipe;
+export default SearchRecipe
