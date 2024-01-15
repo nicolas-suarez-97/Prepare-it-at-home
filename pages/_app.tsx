9b0be145d3f1b1b5a12cb526208064c5a1eb0c61
nicolas-suarez-97 <nicolas.suarez10@hotmail.com>
2024-01-15 18:51:21 -0500

    Adjust fb pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,34 +25,17 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [router.events])
 
   return <>
-    <Script
-        id="fb-pixel"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', ${fbq.FB_PIXEL_ID});
-          `
-        }}
-    />
     <Head>
-      <meta name="facebook-domain-verification" content="c7pns0ybtixl43feet5fxorxqs5juj" />
+      <meta name="facebook-domain-verification" content="c7pns0ybtixl43feet5fxorxqs5juj"/>
       <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
       />
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap" rel="stylesheet"/>
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`} />
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}/>
       <script
-        dangerouslySetInnerHTML={{
-          __html: `
+          dangerouslySetInnerHTML={{
+            __html: `
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
@@ -60,9 +43,33 @@ function MyApp({ Component, pageProps }: AppProps) {
                           page_path: window.location.pathname,
                         });
                     `,
-        }}
+          }}
       />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      <script
+          id="fb-pixel"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', ${fbq.FB_PIXEL_ID});
+              fbq('track', 'PageView');
+            `
+          }}
+      />
+      <img
+          height="1"
+          width="1"
+          style={{display: 'none'}}
+          src={`https://www.facebook.com/tr?id=${fbq.FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          alt="fb_pageview"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     </Head>
     <Component {...pageProps} />
   </>
